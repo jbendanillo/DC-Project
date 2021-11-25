a9a5ff63a1f5ebcff4f946e28e4e3bddde83494e
Jeniza <jeniza.bendanillo@gmail.com>
2021-11-25 15:08:27 +1300

    Adding webdriver, setup and teardown and helper functions

---
+++ Jeniza-DC-Project/Base.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace Jeniza_DC_Project
+{
+    class Base
+    {
+        public static IWebDriver webDriver { get; set; }
+
+        [SetUp]
+        public void SetUp()
+        {
+            //launch the browser
+            webDriver = new ChromeDriver();
+            webDriver.Navigate().GoToUrl("https://www.demo.bnz.co.nz/client/");
+            Thread.Sleep(3000);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            //quit the browser
+            webDriver.Quit();
+        }
+    }
+}
---
+++ Jeniza-DC-Project/HelperFunctions.cs
@@ -0,0 +1,75 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using static Jeniza_DC_Project.Base;
+
+namespace Jeniza_DC_Project
+{
+    public static class HelperFunctions
+    {
+        public static Boolean CheckIfPayeeExist(string name)
+        {
+            // Get all payees from the list and find the newly added payee
+            IList<IWebElement> payees = webDriver.FindElements(By.TagName("li"));
+            for (int i = 0; i < payees.Count; i++)
+            {
+                var text = payees.ElementAt(i).FindElement(By.ClassName("js-payee-name")).Text;
+                if (text == name)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static Boolean CheckIfListAscending()
+        {
+            // Get list of all payee names
+            IList<IWebElement> payees = webDriver.FindElements(By.TagName("li"));
+            var listPayees = new List<string>();
+            for (int i = 0; i < payees.Count; i++)
+            {
+                var payee = payees.ElementAt(i).FindElement(By.ClassName("js-payee-name")).Text;
+                listPayees.Add(payee);
+            }
+
+            // Create list of original ascending list
+            var unsortedListPayees = new List<string>(listPayees);
+
+            // Sort list ascending
+            var sortedListPayees = new List<string>(listPayees);
+            sortedListPayees.Sort((a, b) => a.CompareTo(b));
+
+            // Check if list ascending by comparing the original and the new sorted list
+            var isAscending = listPayees.SequenceEqual(sortedListPayees);
+            if (isAscending) { return true; } else { return false; };
+
+        }
+
+        public static Boolean CheckIfListDescending()
+        {
+            // Get list of payee names
+            IList<IWebElement> payees = webDriver.FindElements(By.TagName("li"));
+            var listPayees = new List<string>();
+            for (int i = 0; i < payees.Count; i++)
+            {
+                var payee = payees.ElementAt(i).FindElement(By.ClassName("js-payee-name")).Text;
+                listPayees.Add(payee);
+            }
+
+            // create list of original ascending list
+            var unsortedListPayees = new List<string>(listPayees);
+            var sortedListPayees = new List<string>(listPayees);
+
+            // sort list descending
+            sortedListPayees.Sort((a, b) => b.CompareTo(a));
+
+            // check if list descending by comparing the original and the new sorted list
+            var isDescending = sortedListPayees.SequenceEqual(unsortedListPayees);
+            if (isDescending) { return true; } else { return false; };
+
+        }
+    }
+}
